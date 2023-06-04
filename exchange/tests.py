f1d892946075b2f9ea538c0020fee4db718c1992
Pixel2022UA <zelenskiy2013@gmail.com>
2023-06-04 19:17:13 +0300

    add tests

--- exchange/tests.py
+++ exchange/tests.py
@@ -1,18 +1,25 @@
 import json
+import os
 import pathlib
 from datetime import date
 
 import pytest
 import responses
 from django.core.management import call_command
+from dotenv import load_dotenv
 from freezegun import freeze_time
 
-from .exchange_provider import MonoExchange, PrivatExchange
+from .exchange_provider import MonoExchange, PrivatExchange, NacBankExchange, OpenExchange, LayerExchange
 from .models import Rate
 from .views import index
 
 root = pathlib.Path(__file__).parent
 
+load_dotenv()
+
+openexch_api = os.getenv("API_OpenExch")
+layer_api = os.getenv("API_ApiLayer")
+headers = {"apikey": layer_api}
 
 @pytest.fixture
 def mocked():
@@ -44,6 +51,29 @@ def test_privat_rate(mocked):
     e = PrivatExchange("privat", "USD", "UAH")
     e.get_rate()
     assert e.pair.sell == 37.45318
+
+@responses.activate
+def test_nacbank_rate(mocked):
+    mocked_response = mocked("nacbank_response.json")
+    responses.get(
+        "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json",
+        json=mocked_response,
+    )
+    e = NacBankExchange("nacbank", "USD", "UAH")
+    e.get_rate()
+    assert e.pair.sell == 36.5686
+
+@responses.activate
+def test_openexch_rate(mocked):
+    mocked_response = mocked("openexch_response.json")
+    responses.get(
+        f"https://openexchangerates.org/api/latest.json?app_id={openexch_api}&base=USD&symbols=UAH",
+        json=mocked_response,
+    )
+    e = OpenExchange("openexch", "USD", "UAH")
+    e.get_rate()
+    assert e.pair.sell == 37.073792
+
 
 
 @pytest.mark.django_db
